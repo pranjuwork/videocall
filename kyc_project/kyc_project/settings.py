fdd06dfd7f8e639f9876845d7e7dc59de0b5e58f
pranjuwork <pranjal.prabhu@airpay.technology>
2025-07-08 13:13:51 +0530

    Update settings.py

--- kyc_project/kyc_project/settings.py
+++ kyc_project/kyc_project/settings.py
@@ -1,23 +1,30 @@
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
+import dj_database_url
+ 
+# ----------------------------
+# Base Directory
+# ----------------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vg^f@s9#i@uhd!*v4u3823clp17qgrs@mzwap^a^^&7h38ds_#'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
+ 
+# ----------------------------
+# Secret Key
+# ----------------------------
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-for-local')
+ 
+# ----------------------------
+# Debug Mode
+# ----------------------------
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+ 
+# ----------------------------
+# Allowed Hosts
+# ----------------------------
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ 
+# ----------------------------
+# Installed Apps
+# ----------------------------
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -25,11 +32,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'kyc_app',  
+    'kyc_app',
+    'whitenoise.runserver_nostatic',  # for serving static in prod
 ]
-
+ 
+# ----------------------------
+# Middleware
+# ----------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # for static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -37,13 +49,20 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+ 
+# ----------------------------
+# Root URLs & WSGI
+# ----------------------------
 ROOT_URLCONF = 'kyc_project.urls'
-
+WSGI_APPLICATION = 'kyc_project.wsgi.application'
+ 
+# ----------------------------
+# Templates
+# ----------------------------
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # if you use a global template dir
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -54,64 +73,67 @@ TEMPLATES = [
         },
     },
 ]
-
-WSGI_APPLICATION = 'kyc_project.wsgi.application'
-
-
+ 
+# ----------------------------
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+# ----------------------------
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
+ 
+# ----------------------------
+# Password Validators
+# ----------------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
-
-
+ 
+# ----------------------------
 # Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
+# ----------------------------
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
+ 
+# ----------------------------
+# Static and Media Files
+# ----------------------------
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'kyc_app/static']
+ 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
  
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+ 
+# ----------------------------
+# Auth Redirects
+# ----------------------------
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
+ 
+# ----------------------------
+# Security for Production
+# ----------------------------
+if not DEBUG:
+    CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS if host != '*']
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_SSL_REDIRECT = True
+ 
+# ----------------------------
+# Default Auto Field
+# ----------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
